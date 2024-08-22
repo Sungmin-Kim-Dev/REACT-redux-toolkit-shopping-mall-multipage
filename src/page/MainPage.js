72121ba62079a40d867f47ed70b456373267aa71
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-08-23 02:17:57 +0900

    fake json server setting

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -5,7 +5,8 @@ import ItemCard from "../component/ItemCard";
 const MainPage = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = `http://localhost:3004/products/`;
+    // let url = `http://localhost:3004/products/`;
+    let url = `http://my-json-server.typicode.com/Sungmin-Kim-Dev/REACT-shopping-mall-multipage/products/`;
     let response = await fetch(url);
     let data = await response.json();
     // console.table(data);
